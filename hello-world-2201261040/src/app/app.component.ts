31f9598a1e7171bfd778c8d61366bfb776cd8dbc
Emo <emo.2012012@gmail.com>
2024-04-25 20:57:41 +0300

    Add my personal info

--- hello-world-2201261040/src/app/app.component.ts
+++ hello-world-2201261040/src/app/app.component.ts
@@ -1,13 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'hello-world-2201261040';
+  title = 'The best Youtuber';
+  name = 'Emo';
+  hobbies = ['Четене на книги', 'Бягане', 'Колоездене'];
+  favoriteContentCreator = {
+    name: 'PewDiePie',
+    link: 'https://www.youtube.com/user/pewdiepie',
+  };
+  channelInfo = {
+    name: 'Emo channel',
+    description: 'I am a BG youtuber',
+    influence: 'I make people happy',
+  };
+  services = [
+    {
+      name: 'Video editing',
+      price: 20,
+    },
+    {
+      name: 'Ads shoot',
+      price: 150,
+    },
+    {
+      name: 'Account manager',
+      price: 350,
+    },
+  ];
 }
